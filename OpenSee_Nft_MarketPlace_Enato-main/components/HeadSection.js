087eff26fcf10e4443d1b3922e1c7c0c6aafc838
devalenya <31144181+devalenya@users.noreply.github.com>
2022-02-23 08:04:15 +0100

    Update HeadSection.js

--- OpenSee_Nft_MarketPlace_Enato-main/components/HeadSection.js
+++ OpenSee_Nft_MarketPlace_Enato-main/components/HeadSection.js
@@ -3,6 +3,12 @@ import Link from "next/link"
 import { useState,useEffect } from 'react'
 import Web3 from "web3"
 import detectEthereumProvider from '@metamask/detect-provider'
+import { PolyjuiceHttpProvider } from '@polyjuice-provider/web3';
+
+import { AddressTranslator } from 'nervos-godwoken-integration';
+
+import { CONFIG } from '../config';
+
 
 const HeadSection = () => {
    const[web3Api,setWe3Api] = useState({
